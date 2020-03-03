5422c7aebfca9cacbea2f60faa0082b69f9a431c
Jeremy Custenborder <jcustenborder@gmail.com>
2020-03-03 10:12:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Introduction
+
+This is a template repository for creating Kafka Connect Plugins. 
+
+
+
---
+++ src/main/java/com/github/jcustenborder/kafka/connect/example/package-info.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright © 2019 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@Introduction("This is information about the connector.")
+@Title("Title")
+@DocumentationWarning("This is a warning")
+@PluginOwner("jcustenborder")
+@PluginName("kafka-connect-example")
+package com.github.jcustenborder.kafka.connect.example;
+
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationWarning;
+import com.github.jcustenborder.kafka.connect.utils.config.Introduction;
+import com.github.jcustenborder.kafka.connect.utils.config.PluginName;
+import com.github.jcustenborder.kafka.connect.utils.config.PluginOwner;
+import com.github.jcustenborder.kafka.connect.utils.config.Title;
